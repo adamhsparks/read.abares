af288023d4c00566b4a773209dd2f939bd36f1a3
Adam H. Sparks <adamhsparks@icloud.com>
2024-09-15 14:55:16 +0800

    tidy up documentation and arguments for get_agfd()

--- R/get_agfd.R
+++ R/get_agfd.R
@@ -1,19 +1,27 @@
+
 #' Fetch Australian Gridded Farm Data
 #'
-#' @param fixed `Boolean` Download fixed historical climate prices?  Defaults
-#'  to `FALSE`, downloads the data from ABARES \sQuote{farmpredict} (Hughes
-#'  *et al.* 2022). Using `TRUE` will download simulations where global output
-#'  and input price indexes are fixed at values from the most recently completed
-#'  financial year.
+#' @param fixed_prices `Boolean` Download historical climate and prices or
+#'  historical climate and fixed prices as described in  (Hughes *et al.* 2022).
+#'  Defaults to `TRUE` and downloads the data with historical climate and fixed
+#'  prices \dQuote{to isolate the effects of climate variability on financial
+#'  incomes for broadacre farm businesses} (ABARES 2024). Using `TRUE` will
+#'  download simulations where global output and input price indexes are fixed
+#'  at values from the most recently completed financial year.
 #' @param cache `Boolean` Cache the Australian Gridded Farm Data files after
 #'  download using [tools::R_user_dir] to identify the proper directory for
 #'  storing user data in a cache for this package. Defaults to `TRUE`, caching
 #'  the files locally. If `FALSE`, this function uses `tempdir()` and the files
 #'  are deleted upon closing of the \R session.
 #'
-#' @references N. Hughes, W.Y. Soh, C. Boult, K. Lawson, Defining drought from
-#'  the perspective of Australian farmers, Climate Risk Management, Volume 35,
-#'  2022, 100420, ISSN 2212-0963, DOI
+#' @references
+#' *Australian gridded farm data*, Australian Bureau of Agricultural and
+#'  Resource Economics and Sciences, Canberra, July, DOI:
+#'  [10.25814/7n6z-ev41](https://doi.org/10.25814/7n6z-ev41). CC BY 4.0.
+#'
+#' N. Hughes, W.Y. Soh, C. Boult, K. Lawson, *Defining drought from
+#'  the perspective of Australian farmers*, Climate Risk Management, Volume 35,
+#'  2022, 100420, ISSN 2212-0963, DOI:
 #'  [10.1016/j.crm.2022.100420](https://doi.org/10.1016/j.crm.2022.100420).
 #'
 #' @examplesIf interactive()
@@ -24,7 +32,7 @@
 #'
 #' @export
 
-get_agfd <- function(fixed = FALSE, cache = TRUE) {
+get_agfd <- function(fixed_prices = TRUE, cache = TRUE) {
   agfd_zip <- data.table::fifelse(cache,
                                   file.path(.find_user_cache(), "agfd.zip"),
                                   file.path(file.path(tempdir(), "agfd.zip")))
@@ -34,7 +42,7 @@ get_agfd <- function(fixed = FALSE, cache = TRUE) {
 
   # this is where the zip files are unzipped in `agfd_zip_dir`
   agfd_nc_dir <- data.table::fifelse(
-    fixed,
+    fixed_prices,
     file.path(agfd_zip_dir, "historical_climate_prices_fixed"),
     file.path(agfd_zip_dir, "historical_climate_and_prices")
   )
@@ -47,7 +55,7 @@ get_agfd <- function(fixed = FALSE, cache = TRUE) {
     }
 
     url <- data.table::fifelse(
-      fixed,
+      fixed_prices,
       "https://daff.ent.sirsidynix.net.au/client/en_AU/search/asset/1036161/3",
       "https://daff.ent.sirsidynix.net.au/client/en_AU/search/asset/1036161/2"
     )
