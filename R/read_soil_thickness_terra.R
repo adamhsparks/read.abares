88047f686528f7b4fdc027972f6b6867998c8794
Adam H. Sparks <adamhsparks@icloud.com>
2024-09-21 13:35:46 +0800

    Fix `read_soil_thickness_terra()`

---
+++ R/read_soil_thickness_terra.R
@@ -0,0 +1,24 @@
+
+#' Read Soil Thickness File With terra
+#'
+#' Read Soil Thickness for Australian Areas of Intensive Agriculture of Layer 1
+#'  data as [terra::rast] objects.
+#'
+#' @param files An \pkg{abares} `abares.soil.thickness` object, a `list` that
+#'  contains the \acronym{ESRI} grid file to import
+#' @return a [terra::rast] object of the Soil Thickness for Australian Areas of
+#'  Intensive Agriculture of Layer 1
+#'
+#' @examplesIf interactive()
+#' get_soil_thickness(cache = TRUE) |>
+#'   read_soil_thickness_terra()
+#'
+#' @family soil_thickness
+#' @autoglobal
+#' @export
+
+read_soil_thickness_terra <- function(files) {
+  .check_class(x = files, class = "abares.soil.thickness.files")
+  r <- terra::rast(files$grid)
+  return(r)
+}
