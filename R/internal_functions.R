b0107a4725776d0f47210768a0b5cf6e1a038a73
Adam H. Sparks <adamhsparks@icloud.com>
2024-09-21 13:36:00 +0800

    add internal function to check S3 classes

--- R/internal_functions.R
+++ R/internal_functions.R
@@ -1,4 +1,5 @@
 
+
 #' Find the File Path to Users' Cache Directory
 #'
 #' @return A `character` string value of a file path indicating the proper
@@ -7,4 +8,21 @@
 #' @keywords Internal
 .find_user_cache <- function() {
   tools::R_user_dir(package = "abares", which = "cache")
+}
+
+#' Check for abares.soil.thickness Class
+#'
+#' @param x An object for validating
+#' @param class An S3 class to validate against
+#'
+#' @return Nothing, called for its side-effects of class validation
+#' @keywords Internal
+#' @noRd
+
+.check_class <- function(x, class) {
+  if (missing(x) || !inherits(x, class)) {
+    cli::cli_abort(
+      message = "You must provide an {.code class} object."
+    )
+  }
 }
