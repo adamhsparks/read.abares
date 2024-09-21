d80c922cb404dcad72e48bf880dd7aff180945fa
Adam H. Sparks <adamhsparks@icloud.com>
2024-09-21 13:40:09 +0800

    add stars to soil thickness

---
+++ R/read_soil_thickness_stars.R
@@ -0,0 +1,24 @@
+
+#' Read Soil Thickness File With stars
+#'
+#' Read Soil Thickness for Australian Areas of Intensive Agriculture of Layer 1
+#'  data as a [stars] object.
+#'
+#' @param files An \pkg{abares} `abares.soil.thickness` object, a `list` that
+#'  contains the \acronym{ESRI} grid file to import
+#' @return a [stars] object of the Soil Thickness for Australian Areas of
+#'  Intensive Agriculture of Layer 1
+#'
+#' @examplesIf interactive()
+#' get_soil_thickness(cache = TRUE) |>
+#'   read_soil_thickness_stars()
+#'
+#' @family soil_thickness
+#' @autoglobal
+#' @export
+
+read_soil_thickness_stars <- function(files) {
+  .check_class(x = files, class = "abares.soil.thickness.files")
+  s <- stars::read_stars(files$grid)
+  return(s)
+}
