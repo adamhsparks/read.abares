8bc402b55e8f2e5c5aa08c2f0d3af848f88f323c
Adam H. Sparks <adamhsparks@icloud.com>
2024-10-26 22:43:43 +0800

    improved test coverage for inspect_cache()

--- tests/testthat/test-inspect_cache.R
+++ tests/testthat/test-inspect_cache.R
@@ -1,6 +1,19 @@
 
 withr::local_envvar(R_USER_CACHE_DIR = file.path(tempdir()))
 dir.create(file.path(.find_user_cache()), recursive = TRUE)
+# check message with no files present ----
+
+test_that("inspect_cache() works w/ no files", {
+  cli_out <- function() {
+    return(cli::cli_inform(c(
+      "There do not appear to be any files cached for {.pkg {{read.abares}}}."
+    )))
+  }
+
+  expect_identical(inspect_cache(), cli_out())
+})
+
+# Now create a file to check when files are present ----
 test_file <- file.path(.find_user_cache(), "test.R")
 test_that("inspect_cache() works, recursive = FALSE", {
   file.create(test_file)
@@ -8,9 +21,7 @@ test_that("inspect_cache() works, recursive = FALSE", {
   f <- .find_user_cache()
   f <- list.files(f, full.names = TRUE)
   expect_identical(inspect_cache() |>
-                     capture_output(),
-                   f |> capture_output()
-  )
+                     capture_output(), f |> capture_output())
 })
 
 test_that("inspect_cache() works, recursive = TRUE", {
@@ -20,8 +31,7 @@ test_that("inspect_cache() works, recursive = TRUE", {
   f <- list.files(f, recursive = TRUE, full.names = TRUE)
   expect_identical(inspect_cache(recursive = TRUE) |>
                      capture_output(),
-                   f |> capture_output()
-  )
+                   f |> capture_output())
 })
 
 withr::deferred_run()
