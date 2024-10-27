3fb28eaf1329a77cacf6b994e8cdc968a4f522ab
Adam H. Sparks <adamhsparks@icloud.com>
2024-10-27 16:22:04 +0800

    redocument

--- R/globals.R
+++ R/globals.R
@@ -2,6 +2,7 @@
 
 utils::globalVariables(c(
   "Year_month", # <.download_abares_trade>
+  "Month_issued", # <get_historical_forecast_database>
   "lat", # <read_agfd_dt>
   "lon", # <read_agfd_dt>
   NULL
