e5669b129ca47459793884ddef7188f68de21b88
Adam H. Sparks <adamhsparks@icloud.com>
2025-03-19 21:12:47 +0800

    delete slow_tests, move back to tests

--- tests/slow_tests/test-read_soil_thickness_stars.R
+++
@@ -1,8 +0,0 @@
-test_that("read_soil_thickness_stars returns a stars object", {
-  skip_if_offline()
-  skip_on_ci()
-  x <- get_soil_thickness(cache = TRUE) |>
-    read_soil_thickness_stars()
-  expect_s3_class(x, "stars")
-  expect_named(x, "thpk_1")
-})
--- tests/slow_tests/test-read_soil_thickness_terra.R
+++
@@ -1,8 +0,0 @@
-test_that("read_soil_thickness_stars returns a terra object", {
-  skip_if_offline()
-  skip_on_ci()
-  x <- get_soil_thickness(cache = TRUE) |>
-    read_soil_thickness_terra()
-  expect_s4_class(x, "SpatRaster")
-  expect_named(x, "thpk_1")
-})
