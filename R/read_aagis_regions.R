9abd5eb1ffec424b62886c0f80e1c691aae37c26
Adam H. Sparks <adam.sparks@curtin.edu.au>
2025-04-01 13:55:35 +0800

    add state col to read_aagis_regions output

--- R/read_aagis_regions.R
+++ R/read_aagis_regions.R
@@ -3,10 +3,13 @@
 #' Download, cache and import the Australian Agricultural and Grazing
 #'  Industries Survey (\acronym{AAGIS}) regions geospatial shapefile.
 #'
-#'  @note Upon import, the geometries are automatically corrected to fix invalid
-#'  geometries that are present in the original shapefile and column names are
-#'  set to start with an upper-case letter and "name" is set to "AAGIS_region"
-#'  to align with the [data.table::data.table()] provided by
+#'  @note Upon import a few operations are carried out,
+#'  * the geometries are automatically corrected to fix invalid geometries that
+#'  are present in the original shapefile,
+#'  * a new column, "State" is added to be used for mapping state estimates with
+#'  data provided by [read_historical_state_estimates()], and
+#'  * column names are set to start with an upper-case letter and "name" is set
+#'  to "AAGIS_region" to align with the [data.table::data.table()] provided by
 #'  [read_historical_regional_estimates()] to allow for easier merging of data.
 #'
 #' @param cache Cache the \acronym{AAGIS} regions' geospatial file after
@@ -21,7 +24,8 @@
 #'
 #' plot(aagis)
 #'
-#' @returns An \CRANpkg{sf} object of the \acronym{AAGIS} regions.
+#' @returns An \CRANpkg{sf} object of the \acronym{AAGIS} regions and a
+#' cached geopackage file if requested.
 #'
 #' @family AAGIS
 #'
@@ -88,6 +92,8 @@ read_aagis_regions <- function(cache = TRUE) {
   # From checking the unzipped file, some geometries are invalid, this corrects
   aagis_sf <- sf::st_make_valid(aagis_sf)
   aagis_sf["aagis"] <- NULL # drop an identical column with class
+  # pull the state codes out and create a new state column
+  aagis_sf$State <- gsub(" .*$", "", aagis_sf$name)
   names(aagis_sf)[names(aagis_sf) == "name"] <- "ABARES_region"
   names(aagis_sf)[names(aagis_sf) == "class"] <- "Class"
   names(aagis_sf)[names(aagis_sf) == "zone"] <- "Zone"
