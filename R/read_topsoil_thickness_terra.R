9c9de24c6118905dc2f9c950d7a77f76860ccd27
Adam H. Sparks <adamhsparks@icloud.com>
2025-04-14 21:48:20 +0800

    fix file paths for topsoil files

--- R/read_topsoil_thickness_terra.R
+++ R/read_topsoil_thickness_terra.R
@@ -28,7 +28,10 @@ read_topsoil_thickness_terra <- function(files) {
   # if we're reading from tempdir() we need to remove the RAT that is removed
   # by default when caching the GTiff
   if (!grepl(pattern = .find_user_cache(), files$GTiff)) {
-    files$GTiff <- terra::init(files$GTiff, files$Gtiff[]) # remove RAT legend
+    files$GTiff <- terra::init(
+      as.character(files$GTiff),
+      as.character(files$Gtiff[])
+    ) # remove RAT legend
   }
-  return(terra::rast(files$GTiff))
+  return(terra::rast(as.character(files$GTiff)))
 }
