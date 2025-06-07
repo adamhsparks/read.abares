5c7583537c6fd90c0b599eedb09e20fbe743d38a
Adam H. Sparks <adamhsparks@icloud.com>
2025-06-07 12:45:18 +0800

    add read.abares-options documentation skeleton

---
+++ R/read.abares-options.R
@@ -0,0 +1,20 @@
+#' Package Options for read.abares
+#'
+#' This page documents the global options used by the \pkg{read.abares} package.
+#'
+#' The following options can be set via [options()] to control package behavior:
+#'
+#' \describe{
+#'   \item{`read.abares.ua`}{String. Set a custom useragent for web requests. Default is `"read.abares"`.}
+#'   \item{`myPackage.timeout`}{Numeric. Timeout in seconds for operations. Default is `30`.}
+#' }
+#'
+#' These options can be set globally using:
+#' ```r
+#' options(myPackage.ua = "myCustomUserAgent")
+#' ```
+#'
+#' @seealso [options()], [getOption()]
+#' @name read.abares-options
+#' @keywords internal
+NULL
