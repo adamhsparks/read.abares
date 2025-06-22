03908eb5422f6424128ff501e38be64729a51960
Adam H. Sparks <adamhsparks@icloud.com>
2025-06-22 14:49:34 +0800

    document version string in options

--- R/read.abares-options.R
+++ R/read.abares-options.R
@@ -7,7 +7,7 @@
 #' \describe{
 #'   \item{`read.abares.cache`}{Boolean globally enable or disable caching for select data sets serviced by \pkg{read.abares}. Defaults to `FALSE` not caching data locally between \R sessions.}
 #'   \item{`read.abares.cache_location`}{Character string to set a custom location for caching data sets serviced by \pkg{read.abares}. Defaults to `tools::R_user_dir(package = "read.abares", which = "cache")`.}
-#'   \item{`read.abares.user_agent`}{Character string to set a custom user agent for web requests. Default is `"read.abares"`.}
+#'   \item{`read.abares.user_agent`}{Character string to set a custom user agent for web requests. Default is `read.abares R package {version no.} https://github.com/adamhsparks/read.abares`.}
 #'   \item{`read.abares.timeout`}{Integer providing the timeout in seconds for download operations. Default is `2000`.}
 #'   \item{`read.abares.max_tries`}{Integer providing the number of times to retry download before giving up. Default is `3`.}
 #'   \item{`read.abares.quiet`}{Boolean globally enable or disable download status bars and other messages, but not errors. Defaults to `FALSE` providing user-feedback.}
