e878681e223e3365a6961912af0bece40e22d0ca
Adam H. Sparks <adamhsparks@icloud.com>
2025-06-28 20:27:59 +0800

    clean up documentation and fix internal bugs

--- R/get_agfd.R
+++ R/get_agfd.R
@@ -4,20 +4,14 @@
 #'  Gridded Farm Data} (\acronym{AGFD}) data and unzips the compressed files to
 #'  NetCDF for importing.
 #'
-#' @param fixed_prices Download historical climate and prices or historical
+#' @param .fixed_prices Download historical climate and prices or historical
 #'  climate and fixed prices as described in (Hughes *et al.* 2022).
-#' @param yyyy Returns only data for the specified year or years for climate
+#' @param .yyyy Returns only data for the specified year or years for climate
 #'  data (fixed prices) or the years for historical climate and prices depending
-#'  upon the setting of `fixed_prices`.  Note that this will still download the
+#'  upon the setting of `.fixed_prices`.  Note that this will still download the
 #'  entire data set, that cannot be avoided, but will only return the
 #'  requested year(s) in your \R session.  Valid years are from 1991 to 2023
 #'  inclusive.
-#' @param cache Boolean cache the files after download?  Defaults to `FALSE`
-#'  with files being downloaded to `tempdir()` being available throughout the
-#'  active \R session and cleaned up on exit. If set to `TRUE`, files will be
-#'  cached locally for use between sessions.
-#' @param cache_location Character string providing the file path to use for
-#'  cached files.
 #'
 #' @examples
 #' agfd <- get_agfd()
@@ -33,8 +27,9 @@
   .fixed_prices,
   .yyyy
 ) {
+  cache <- getOption("read.abares.cache", FALSE)
   download_file <- data.table::fifelse(
-    getOption(read.abares.cache, FALSE),
+    cache,
     fs::path(.find_user_cache(), "agfd.zip"),
     fs::path(tempdir(), "agfd.zip")
   )
@@ -44,7 +39,7 @@
 
   # this is where the zip files are unzipped and read from
   agfd_nc_dir <- data.table::fifelse(
-    fixed_prices,
+    .fixed_prices,
     fs::path(download_dir, "historical_climate_prices_fixed"),
     fs::path(download_dir, "historical_climate_and_prices")
   )
@@ -57,7 +52,7 @@
     }
 
     file_url <- data.table::fifelse(
-      fixed_prices,
+      .fixed_prices,
       "https://daff.ent.sirsidynix.net.au/client/en_AU/search/asset/1036161/3",
       "https://daff.ent.sirsidynix.net.au/client/en_AU/search/asset/1036161/2"
     )
@@ -90,8 +85,8 @@
 
   agfd_nc <- fs::dir_ls(agfd_nc_dir, full.names = TRUE)
 
-  if (isFALSE(missing(yyyy))) {
-    yyyy <- sprintf("c%d", yyyy)
+  if (isFALSE(missing(.yyyy))) {
+    yyyy <- sprintf("c%d", .yyyy)
     agfd_nc <- agfd_nc[grepl(paste(yyyy, collapse = "|"), names(agfd_nc))]
   }
   return(agfd_nc)
