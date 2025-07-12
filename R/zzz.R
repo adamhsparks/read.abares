360603fed96426af909e75d0a23f2f230aef9756
Adam H. Sparks <adamhsparks@icloud.com>
2025-07-12 19:49:25 +0800

    enhanced verbosity settings, saves other pkg options and resets on exit

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,18 @@
 .onLoad <- function(libname, pkgname) {
+  # save options for non-read.abares options for resetting after package exits
   op <- options()
+  read.abares_env <- new.env(parent = emptyenv())
+  read.abares_env$old_options <- op[
+    names(op) %in%
+      c(
+        "rlib_message_verbosity",
+        "rlib_warning_verbosity",
+        "warn",
+        "datatable.showProgress"
+      )
+  ]
+  assign(".read.abares_env", read.abares_env, envir = parent.env(environment()))
+
   op.read.abares <- list(
     read.abares.cache = FALSE,
     read.abares.cache_location = tools::R_user_dir(
@@ -20,7 +33,7 @@
   rlib_message_level <- switch(
     verbosity,
     "quiet" = "quiet",
-    "minimal" = "quiet",
+    "minimal" = "minimal",
     "verbose" = "verbose",
     "verbose"
   )
@@ -31,8 +44,32 @@
     "verbose" = "verbose",
     "verbose"
   )
+  warn_level <- switch(
+    verbosity,
+    "quiet" = -1L,
+    "minimal" = 0L,
+    "verbose" = 0L
+  )
+  fread_level <- switch(
+    verbosity,
+    "quiet" = FALSE,
+    "minimal" = FALSE,
+    "verbose" = TRUE
+  )
   options(
     rlib_message_verbosity = rlib_message_level,
-    rlib_warning_verbosity = rlib_warning_level
+    rlib_warning_verbosity = rlib_warning_level,
+    warn = warn_level,
+    datatable.showProgress = fread_level
   )
+}
+
+.onUnload <- function(libpath) {
+  if (exists(".read.abares_env", envir = parent.env(environment()))) {
+    old_opts <- get(
+      ".read.abares_env",
+      envir = parent.env(environment())
+    )$old_options
+    options(old_opts)
+  }
 }
