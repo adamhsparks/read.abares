249ed5b3766fc7375da4f595bd210780c389f4e6
Adam H. Sparks <adamhsparks@icloud.com>
2025-07-21 21:02:31 +0800

    simplify topsoil tests (still not passing tho)

--- tests/testthat/test-topsoil_thickness_functions.R
+++ tests/testthat/test-topsoil_thickness_functions.R
@@ -1,58 +1,33 @@
-# There are two files for this function due to caching tests
+# Test with no caching enabled
 
-# sets up a custom cache environment in `tempdir()` just for testing
-withr::local_envvar(R_USER_CACHE_DIR = tempdir())
+withr::local_options(read.abares.cache_location = tempdir())
+withr::local_options(read.abares.cache = FALSE)
 
-# without caching enabled ----
+# test reading with stars ----
 
-test_that(".get_topsoil_thickness doesn't cache", {
+test_that("read_topsoil_thickness_stars returns a stars object", {
   skip_if_offline()
   skip_on_ci()
-  x <- .get_topsoil_thickness()
-  expect_s3_class(x, c("read.abares.topsoil.thickness", "list"))
-  expect_false(fs::dir_exists(fs::path(
-    .find_user_cache(),
-    "topsoil_thickness_dir"
-  )))
-  # cleanup on the way out for the next test
-  expect_message(clear_cache())
+  x <- read_topsoil_thickness_stars()
+  expect_s3_class(x, "stars")
+  expect_named(x, "thpk_1.tif")
 })
 
+# test reading with terra ----
 
-# with caching enabled after is was not initially enabled ----
-
-test_that("get_topsoil_thickness caches", {
+test_that("read_topsoil_thickness_terra returns a terra object", {
   skip_if_offline()
   skip_on_ci()
-  temp_cache <- fs::path(tempdir(), "R/read.abares/")
-  withr::local_options(
-    list(read.abares.cache_location = temp_cache),
-    getOption("read.abares.cache_location")
-  )
-  withr::local_options(
-    list(read.abares.cache = TRUE),
-    getOption("read.abares.cache")
-  )
-  x <- .get_topsoil_thickness()
-  expect_s3_class(x, c("read.abares.topsoil.thickness", "list"))
-  expect_true(fs::file_exists(fs::path(
-    .find_user_cache(),
-    "topsoil_thickness_dir"
-  )))
-  expect_no_message(clear_cache())
-  withr::deferred_run()
+  x <- read_topsoil_thickness_terra()
+  expect_s4_class(x, "SpatRaster")
+  expect_named(x, "thpk_1")
 })
 
-test_that("get_topsoil_thickness does cache", {
-  skip_if_offline()
-  skip_on_ci()
-  x <- .get_topsoil_thickness()
-  expect_s3_class(x, c("read.abares.topsoil.thickness", "list"))
-  expect_true(fs::file_exists(fs::path(
-    .find_user_cache(),
-    "topsoil_thickness_dir"
-  )))
-})
+withr::deferred_run()
+
+# test with caching enabled
+withr::local_options(read.abares.cache_location = tempdir())
+withr::local_options(read.abares.cache = TRUE)
 
 # test reading with stars ----
 
