e0bcca628181c7309e71c6e5e0fce3476b05ad2b
Adam H. Sparks <adamhsparks@icloud.com>
2025-08-03 21:53:28 +0800

    add new test for 2020-21 nlum data

--- tests/testthat/test-read_nlum_stars.R
+++ tests/testthat/test-read_nlum_stars.R
@@ -3,7 +3,7 @@ test_that("read_nlum_stars() retrieves 2010-11 land use data", {
   skip_on_ci()
   x <- read_nlum_stars(data_set = "Y201011")
   expect_s3_class(x, c("stars_proxy", "stars"))
-  expect_identical(basename(names(x)),  "NLUM_v7_250_ALUMV8_2010_11_alb.tif")
+  expect_identical(basename(names(x)), "NLUM_v7_250_ALUMV8_2010_11_alb.tif")
 })
 
 test_that("read_nlum_stars() retrieves 2015-16 land use data", {
@@ -11,5 +11,13 @@ test_that("read_nlum_stars() retrieves 2015-16 land use data", {
   skip_on_ci()
   x <- read_nlum_stars(data_set = "Y201516")
   expect_s3_class(x, c("stars_proxy", "stars"))
-  expect_identical(basename(names(x)),  "NLUM_v7_250_ALUMV8_2015_16_alb.tif")
+  expect_identical(basename(names(x)), "NLUM_v7_250_ALUMV8_2015_16_alb.tif")
+})
+
+test_that("read_nlum_stars() retrieves 2020-21 land use data", {
+  skip_if_offline()
+  skip_on_ci()
+  x <- read_nlum_stars(data_set = "Y202021")
+  expect_s3_class(x, c("stars_proxy", "stars"))
+  expect_identical(basename(names(x)), "NLUM_v7_250_ALUMV8_2020_21_alb.tif")
 })
