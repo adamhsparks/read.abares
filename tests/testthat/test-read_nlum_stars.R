93d8faabd47b2b86987332c7e084a1e8cada536a
Adam H. Sparks <adam.sparks@curtin.edu.au>
2025-08-04 16:08:05 +0800

    add more tests for probability grids

--- tests/testthat/test-read_nlum_stars.R
+++ tests/testthat/test-read_nlum_stars.R
@@ -51,3 +51,35 @@ test_that("read_nlum_stars() retrieves thematic 2015-16 land use layers", {
   expect_s3_class(x, c("stars_proxy", "stars"))
   expect_identical(basename(names(x)), "NLUM_v7_250_INPUTS_2015_16_geo.tif")
 })
+
+test_that("read_nlum_stars() retrieves thematic 2020-21 land use layers", {
+  skip_if_offline()
+  skip_on_ci()
+  x <- read_nlum_stars(data_set = "T202021")
+  expect_s3_class(x, c("stars_proxy", "stars"))
+  expect_identical(basename(names(x)), "NLUM_v7_250_INPUTS_2020_21_geo.tif")
+})
+
+test_that("read_nlum_stars() retrieves 2010-11 probability grids", {
+  skip_if_offline()
+  skip_on_ci()
+  x <- read_nlum_stars(data_set = "P201011")
+  expect_s3_class(x, c("stars_proxy", "stars"))
+  expect_length(x, 23L)
+})
+
+test_that("read_nlum_stars() retrieves 2015-16 probability grids", {
+  skip_if_offline()
+  skip_on_ci()
+  x <- read_nlum_stars(data_set = "P201516")
+  expect_s3_class(x, c("stars_proxy", "stars"))
+  expect_length(x, 23L)
+})
+
+test_that("read_nlum_stars() retrieves 2020-21 probability grids", {
+  skip_if_offline()
+  skip_on_ci()
+  x <- read_nlum_stars(data_set = "P202021")
+  expect_s3_class(x, c("stars_proxy", "stars"))
+  expect_length(x, 23L)
+})
