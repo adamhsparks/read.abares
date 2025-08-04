a9f438672b81155d4aadc570e37f34fab71f05f6
Adam H. Sparks <adam.sparks@curtin.edu.au>
2025-08-04 14:54:16 +0800

    fix tests

--- tests/testthat/test-read_nlum_stars.R
+++ tests/testthat/test-read_nlum_stars.R
@@ -25,7 +25,7 @@ test_that("read_nlum_stars() retrieves 2020-21 land use data", {
 test_that("read_nlum_stars() retrieves change use change data", {
   skip_if_offline()
   skip_on_ci()
-  x <- read_nlum_stars(data_set = "C201021")
+  x <- read_nlum_stars(data_set = "C201121")
   expect_s3_class(x, c("stars_proxy", "stars"))
   expect_identical(
     basename(names(x)),
@@ -36,16 +36,18 @@ test_that("read_nlum_stars() retrieves change use change data", {
   )
 })
 
-test_that("read_nlum_stars() retrieves thematic land use layers", {
+test_that("read_nlum_stars() retrieves thematic 2010-11 land use layers", {
   skip_if_offline()
   skip_on_ci()
   x <- read_nlum_stars(data_set = "T201011")
   expect_s3_class(x, c("stars_proxy", "stars"))
-  expect_identical(
-    basename(names(x)),
-    c(
-      "NLUM_v7_250_SIMP_CHANGE_DETAIL_2011_to_2021_alb.tif",
-      "NLUM_v7_250_SIMP_CHANGE_SUMMARY_2011_to_2021_alb.tif"
-    )
-  )
+  expect_identical(basename(names(x)), "NLUM_v7_250_INPUTS_2010_11_geo.tif")
+})
+
+test_that("read_nlum_stars() retrieves thematic 2015-16 land use layers", {
+  skip_if_offline()
+  skip_on_ci()
+  x <- read_nlum_stars(data_set = "T201516")
+  expect_s3_class(x, c("stars_proxy", "stars"))
+  expect_identical(basename(names(x)), "NLUM_v7_250_INPUTS_2015_16_geo.tif")
 })
