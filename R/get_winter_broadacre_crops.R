e07ae7b0f2093ebd4996d9c97d19f834754016f0
Adam H. Sparks <adam.sparks@curtin.edu.au>
2025-08-20 17:26:59 +0800

    start adding ability to select other crops and years

--- R/get_winter_broadacre_crops.R
+++ R/get_winter_broadacre_crops.R
@@ -1,16 +1,34 @@
 #' Get Winter Broadacre Crops by Australia, State and Territory by Year
 #'
+#' Technically these are from the Australian Bureau of Statistics (\acronym{ABS}
+#' data, not \acronym{ABARES}, but the data is agricultural and so it's serviced
+#' here.
+#'
+#' @param crops A character vector providing the desired cropping data, one of:
+#'  * winter,
+#'  * summer or
+#'  * sugarcane.
+#' @param year A numeric value providing the year of interest to download.
+#'  Formatted as `202223` or `202324` or just use `"latest"` for the most
+#'  recent release.
 #'
 #' @examples
-#' get_winter_crops()
+#' get_broadacre_crops_data()
 #'
 #' @returns A [data.table::data.table() object of the requested data.
 #' @autoglobal
 #'
 
-get_winter_crops <- function() {
+get_broadacre_crops_data <- function(crops, year = "latest") {
   base_url <- "https://www.abs.gov.au/statistics/industry/agriculture/australian-agriculture-broadacre-crops/"
-  year_url <- "2023-24/"
+
+
+
+ year_url <- switch(year,
+         202223 = "2022-23",
+         202324 = "2023-24",
+         latest = "") 
+
   file_url <- "AABDC_Winter_Broadacre_202324.xlsx"
   winter_crops <- fs::path(tempdir(), "winter_crops")
   .retry_download(
@@ -21,7 +39,7 @@ get_winter_crops <- function() {
 }
 
 
-#' Create a unified data.table object of all sheets in Excel Workbook
+#' Create a Unified data.table Object of All Sheets in an Excel Workbook
 #'
 #' @param filename The name of the Excel workbook for import
 #'
@@ -63,4 +81,15 @@ abares_crop_data <- function(filename) {
   ]
   x[, `Data item` := NULL]
   return(x)
+}
+
+https://www.abs.gov.au/statistics/industry/agriculture/australian-agriculture-broadacre-crops
+
+#' Find Which Financial Years Data are Available for
+.find_years <- function(crops) {
+
+  crop_url = "https://www.abs.gov.au/statistics/industry/agriculture/australian-agriculture-broadacre-crops",
+ text <- htm2text::gettxt(crop_url)
+
+matches <- regmatches(text, gregexpr("\\b\\d{4}-\\d{2}\\b", text))
 }
