f5975315f8f1ae1656a43b3229986706b942d34e
Adam H. Sparks <adamhsparks@icloud.com>
2025-08-23 13:52:10 +0800

    correct docs

--- R/read_topsoil_thickness_stars.R
+++ R/read_topsoil_thickness_stars.R
@@ -1,20 +1,22 @@
-#' Read \dQuote{Soil Thickness for Australian Areas of Intensive Agriculture of Layer 1} with stars
+#' Read "Soil Thickness for Australian Areas of Intensive Agriculture of Layer 1" with stars
 #'
-#' Read \dQuote{Soil Thickness for Australian Areas of Intensive Agriculture of
-#'  Layer 1} data as a \CRANpkg{stars} object.
+#' Read "Soil Thickness for Australian Areas of Intensive Agriculture of Layer
+#'  1" data as a \CRANpkg{stars} object.
+#'
+#' @note This function converts a [terra::rast()] object internally rather than
+#' reading directly from a file.
 #'
 #' @inheritParams read_agfd_dt
-#' @param ... Additional arguments passed to [terra::rast()], for *e.g.*,
-#'  `activeCat` if you wished to set the active category when loading any of the
-#'  available GeoTIFF files that are encoded with a raster attribute table.
+#' @param ... Additional arguments passed to [stars::st_as_stars()], for *e.g.*,
+#'  `att` if you wished to set the active category.
 #'
 #' @references
 #' \url{https://data.agriculture.gov.au/geonetwork/srv/eng/catalog.search#/metadata/faa9f157-8e17-4b23-b6a7-37eb7920ead6}
 #' @source
 #' \url{https://anrdl-integration-web-catalog-saxfirxkxt.s3-ap-southeast-2.amazonaws.com/warehouse/staiar9cl__059/staiar9cl__05911a01eg_geo___.zip}
 #'
-#' @returns A [stars] object of the 'Soil Thickness for Australian Areas of
-#'  Intensive Agriculture of Layer 1'.
+#' @returns A [stars] object of the "Soil Thickness for Australian Areas of
+#'  Intensive Agriculture of Layer 1".
 #'
 #' @examplesIf interactive()
 #' st_stars <- read_topsoil_thickness_stars()
@@ -25,7 +27,7 @@
 #' @autoglobal
 #' @export
 
-read_topsoil_thickness_stars <- function(files = NULL) {
+read_topsoil_thickness_stars <- function(files = NULL, ...) {
   files <- .get_topsoil_thickness(.files = files)
-  stars::read_stars(as.character(files$GTiff))
+  return(stars::st_as_stars(files$data))
 }
