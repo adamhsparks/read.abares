b11abeaee589024767fbf56115d502eeaa373509
Adam H. Sparks <adamhsparks@icloud.com>
2025-09-27 11:26:00 +0800

    add proper tests for .retry_download and skip coverage
    Coverage won't work even though we test it, so we'll just skip the file
    in the coverage report

--- tests/testthat/test-retry_download.R
+++ tests/testthat/test-retry_download.R
@@ -1,11 +1,46 @@
-test_that(".retry_download works with real download", {
-  skip_if_offline()
-  temp_file <- tempfile()
-  url <- "https://httpbin.org/bytes/5" # small, safe test file
+library(withr)
+library(fs)
+library(httptest2)
 
-  options(read.abares.verbosity = "quiet")
-  .retry_download(url, temp_file)
+test_that("retry_download succeeds with mocked download", {
+  skip_on_cran()
 
-  expect_true(file.exists(temp_file))
-  expect_equal(fs::file_info(temp_file)$size, 5L)
+  temp_file <- local_tempfile(fileext = ".csv")
+  defer({
+    if (file_exists(temp_file)) file_delete(temp_file)
+  })
+
+  with_mock_dir("_mock/download-success", {
+    expect_invisible(.retry_download(
+      url = "https://httpbin.org/bytes/5", # small, safe test file
+      .f = temp_file
+    ))
+  })
+
+  expect_true(file_exists(temp_file))
+})
+
+test_that("retry_download retries and fails with mocked errors", {
+  skip_on_cran()
+  skip_on_covr()
+
+  temp_file <- local_tempfile(fileext = ".csv")
+  defer({
+    if (file_exists(temp_file)) file_delete(temp_file)
+  })
+
+  with_mock_dir("_mock/download-fail", {
+    with_options(
+      list(read.abares.max_tries = 2L),
+      expect_error(
+        .retry_download(
+          url = "https://example.com/fail.csv",
+          .f = temp_file
+        ),
+        "All download attempts failed."
+      )
+    )
+  })
+
+  expect_false(file_exists(temp_file))
 })
