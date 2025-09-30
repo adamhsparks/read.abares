65077c2665ee2a87872afa17a81220c847bf6623
Adam H. Sparks <adamhsparks@icloud.com>
2025-09-30 19:45:32 +0800

    fix bug in tests for nonexistent argument

--- tests/testthat/test-read_nlum_terra.R
+++ tests/testthat/test-read_nlum_terra.R
@@ -1,3 +1,73 @@
+.onLoad <- function(libname, pkgname) {
+  # save options for non-read.abares options for resetting after package exits
+  op <- options()
+  read.abares_env <- new.env(parent = emptyenv())
+  read.abares_env$old_options <- op[
+    names(op) %in%
+      c(
+        "rlib_message_verbosity",
+        "rlib_warning_verbosity",
+        "warn",
+        "datatable.showProgress"
+      )
+  ]
+  assign(".read.abares_env", read.abares_env, envir = parent.env(environment()))
+
+  op.read.abares <- list(
+    read.abares.user_agent = readabares_user_agent(),
+    read.abares.timeout = 2000L,
+    read.abares.max_tries = 3L,
+    read.abares.verbosity = "verbose"
+  )
+  toset <- !(names(op.read.abares) %in% names(op))
+  if (any(toset)) {
+    options(op.read.abares[toset])
+  }
+
+  verbosity <- getOption("read.abares.verbosity")
+  rlib_message_level <- switch(
+    verbosity,
+    "quiet" = "quiet",
+    "minimal" = "minimal",
+    "verbose" = "verbose",
+    "verbose"
+  )
+  rlib_warning_level <- switch(
+    verbosity,
+    "quiet" = "quiet",
+    "minimal" = "verbose",
+    "verbose" = "verbose",
+    "verbose"
+  )
+  warn_level <- switch(
+    verbosity,
+    "quiet" = -1L,
+    "minimal" = 0L,
+    "verbose" = 0L
+  )
+  fread_level <- switch(
+    verbosity,
+    "quiet" = FALSE,
+    "minimal" = FALSE,
+    "verbose" = TRUE
+  )
+  options(
+    rlib_message_verbosity = rlib_message_level,
+    rlib_warning_verbosity = rlib_warning_level,
+    warn = warn_level,
+    datatable.showProgress = fread_level
+  )
+}
+
+.onUnload <- function(libpath) {
+  if (exists(".read.abares_env", envir = parent.env(environment()))) {
+    old_opts <- get(
+      ".read.abares_env",
+      envir = parent.env(environment())
+    )$old_options
+    options(old_opts)
+  }
+}
 test_that("reads from a provided local zip file and returns a terra rast object", {
   skip_on_cran()
   skip_if_not_installed("terra")
@@ -25,7 +95,7 @@ test_that("reads from a provided local zip file and returns a terra rast object"
       out <- read_nlum_terra(x = zip_path)
       expect_s4_class(out, "SpatRaster")
     },
-    .get_nlum = function(.data_set = NULL, .proj = NULL, .x = NULL) {
+    .get_nlum = function(.data_set = NULL, .x = NULL) {
       return(tif_path)
     }
   )
@@ -42,7 +112,7 @@ test_that("downloads when x is NULL and reads terra rast object", {
     "dummy.tif"
   )
   fs::dir_create(fs::path_dir(tif_path))
-  r <- terra::rast(nrows = 10, ncols = 10, vals = 1:100)
+  r <- terra::rast(nrows = 10L, ncols = 10L, vals = 1L:100L)
   terra::writeRaster(r, tif_path, overwrite = TRUE)
 
   zip_path <- fs::path(
@@ -59,10 +129,10 @@ test_that("downloads when x is NULL and reads terra rast object", {
 
   testthat::with_mocked_bindings(
     {
-      out <- read_nlum_terra(data_set = "Y202021", proj = "Albers")
+      out <- read_nlum_terra(data_set = "Y202021")
       expect_s4_class(out, "SpatRaster")
     },
-    .get_nlum = function(.data_set = NULL, .proj = NULL, .x = NULL) {
+    .get_nlum = function(.data_set = NULL, .x = NULL) {
       return(tif_path)
     },
     .retry_download = retry_mock,
