dc7a8ad279085b79751a042400e0ba390f6afb69
Adam H. Sparks <adam.sparks@curtin.edu.au>
2025-10-07 16:56:55 +0800

    simplify .retry_download()

--- R/retry_download.R
+++ R/retry_download.R
@@ -4,11 +4,9 @@
 #'  \CRANpkg{httr2} to cache in-session results in the `tempdir()`.
 #'
 #' @param url `Character` The URL being requested.
-#' @param .f `Character` A filepath to be written to local storage.
+#' @param dest `Character` A filepath to be written to local storage.
 #' @param .max_tries `Integer` The number of times to retry a failed download
 #'   before emitting an error message.
-#' @param .initial_delay `Integer` The number of seconds to delay before
-#'   retrying the download.  This increases as the tries increment.
 #'
 #' @examples
 #'
@@ -19,26 +17,64 @@
 #' )
 #'
 #' @returns Called for its side-effects, writes an object to the specified
-#'   director for reading into the active \R session later.
+#'   directory for reading into the active \R session later.
 #' @dev
 
 .retry_download <- function(url, dest, .max_tries = 3L) {
+  # Build base request
   req <- httr2::request(base_url = url) |>
     httr2::req_user_agent("read.abares") |>
-    httr2::req_headers("Accept-Encoding" = "identity") |>
-    httr2::req_headers("Connection" = "Keep-Alive") |>
-    httr2::req_options(http_version = 2, timeout = 2000L) |>
+    httr2::req_headers(
+      "Accept-Encoding" = "identity",
+      "Connection" = "Keep-Alive"
+    ) |>
     httr2::req_retry(max_tries = .max_tries) |>
     httr2::req_cache(path = tempdir())
-  if (getOption("read.abares.verbosity") == "verbose") {
-    req <- httr2::req_progress() |>
-      httr2::req_perform() |>
-      httr2::resp_body_raw() |>
-      brio::write_file_raw(path = dest)
-  } else {
-    req |>
-      httr2::req_perform() |>
-      httr2::resp_body_raw() |>
-      brio::write_file_raw(path = dest)
+
+  # Apply conditional modifications
+  req <- .apply_conditional_options(req, url)
+
+  # Perform request and save
+  req |>
+    httr2::req_perform() |>
+    httr2::resp_body_raw() |>
+    brio::write_file_raw(path = dest)
+}
+
+#' Apply conditional options to httr2 request
+#' @param req An httr2 request object
+#' @param url The URL being requested
+#' @returns Modified httr2 request object
+#' @dev
+.apply_conditional_options <- function(req, url) {
+  # Agriculture.gov.au specific options
+  if (.is_agriculture_url(url)) {
+    req <- req |> httr2::req_options(http_version = 2L, timeout = 2000L)
   }
+
+  # Progress display for verbose mode
+  if (.should_show_progress()) {
+    req <- req |> httr2::req_progress()
+  }
+
+  req
+}
+
+#' Check if URL is from agriculture.gov.au documents
+#' @param url URL to check
+#' @returns Logical
+#' @dev
+.is_agriculture_url <- function(url) {
+  grepl(
+    "https://www\\.agriculture\\.gov\\.au/sites/default/files/documents/",
+    url,
+    fixed = FALSE
+  )
+}
+
+#' Check if progress should be shown
+#' @returns Logical
+#' @dev
+.should_show_progress <- function() {
+  identical(getOption("read.abares.verbosity"), "verbose")
 }
