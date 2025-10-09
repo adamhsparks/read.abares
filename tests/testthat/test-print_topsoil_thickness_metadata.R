2df9c954e856263eb4ab84aebcab78cb340592a0
Adam H. Sparks <adam.sparks@curtin.edu.au>
2025-10-09 13:16:41 +0800

    update topsoil thickness tests

---
+++ tests/testthat/test-print_topsoil_thickness_metadata.R
@@ -0,0 +1,36 @@
+test_that("print_topsoil_thickness_metadata displays complete metadata", {
+  # Create metadata with Custodian marker
+  metadata_content <- paste(
+    "Some initial content",
+    "Custodian: CSIRO Land & Water",
+    "Additional metadata content",
+    "More details about the dataset",
+    sep = "\n"
+  )
+
+  mock_obj <- structure(
+    list(
+      metadata = metadata_content,
+      data = terra::rast(nrows = 2, ncols = 2)
+    ),
+    class = c("read.abares.topsoil.thickness", "list")
+  )
+
+  expect_snapshot({
+    print_topsoil_thickness_metadata(mock_obj)
+  })
+})
+
+test_that("print_topsoil_thickness_metadata returns invisible NULL", {
+  mock_obj <- structure(
+    list(
+      metadata = "Some content\nCustodian: Test\nMore content",
+      data = terra::rast(nrows = 2, ncols = 2)
+    ),
+    class = c("read.abares.topsoil.thickness", "list")
+  )
+
+  returned_obj <- withVisible(print_topsoil_thickness_metadata(mock_obj))
+  expect_null(returned_obj$value)
+  expect_false(returned_obj$visible)
+})
