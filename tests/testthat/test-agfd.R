d84c566368288277e662eb6c27172c70571fd469
Adam H. Sparks <adam.sparks@curtin.edu.au>
2025-10-31 09:07:23 +1000

    add test

--- tests/testthat/test-agfd.R
+++ tests/testthat/test-agfd.R
@@ -5,3 +5,13 @@ test_that(".get_agfd() returns a list object", {
   expect_length(agfd, 2L)
   expect_type(agfd, "character")
 })
+
+
+test_that("read_agfd_dt returns a data.table object", {
+  vcr::use_cassette("_get_agfd", {
+    agfd <- read_agfd_dt(fixed_prices = TRUE, yyyy = 2020:2021)
+  })
+  expect_identical(nrow(agfd), 336654L)
+  expect_identical(ncol(agfd), 44L)
+  expect_s3_class(agfd, "data.table")
+})
