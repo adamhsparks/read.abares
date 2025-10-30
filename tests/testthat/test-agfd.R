4ccda6792e2998a38bad80e32dc4f369c60c8fde
Adam H. Sparks <adam.sparks@curtin.edu.au>
2025-10-31 08:52:23 +1000

    adds first test for get_agfd

--- tests/testthat/test-agfd.R
+++ tests/testthat/test-agfd.R
@@ -1,8 +1,7 @@
 test_that(".get_agfd() returns a list object", {
-  skip_if_offline()
   vcr::use_cassette("_get_agfd", {
     agfd <- .get_agfd(.fixed_prices = TRUE, .yyyy = 2020:2021)
   })
-
-  expect_type(agfd, "list")
+  expect_length(agfd, 2L)
+  expect_type(agfd, "character")
 })
