915765126e51954cb063fad27de16a84f48694b3
Adam H. Sparks <adam.sparks@curtin.edu.au>
2025-10-31 10:00:11 +1000

    rename fixture and add more tests

--- tests/testthat/test-agfd.R
+++ tests/testthat/test-agfd.R
@@ -1,5 +1,5 @@
 test_that(".get_agfd() returns a list object", {
-  vcr::use_cassette("_get_agfd", {
+  vcr::use_cassette("_get_agfd_fixed_prices", {
     agfd <- .get_agfd(.fixed_prices = TRUE, .yyyy = 2020:2021)
   })
   expect_length(agfd, 2L)
@@ -7,7 +7,7 @@ test_that(".get_agfd() returns a list object", {
 })
 
 test_that("read_agfd_dt returns a data.table object", {
-  vcr::use_cassette("_get_agfd", {
+  vcr::use_cassette("_get_agfd_fixed_prices", {
     agfd <- read_agfd_dt(fixed_prices = TRUE, yyyy = 2020:2021)
   })
   expect_identical(nrow(agfd), 336654L)
@@ -15,11 +15,18 @@ test_that("read_agfd_dt returns a data.table object", {
   expect_s3_class(agfd, "data.table")
 })
 
-
 test_that("read_agfd_stars returns a list of stars objects", {
-  vcr::use_cassette("_get_agfd", {
+  vcr::use_cassette("_get_agfd_fixed_prices", {
     agfd <- read_agfd_stars(fixed_prices = TRUE, yyyy = 2020:2021)
   })
   expect_length(agfd, 2L)
-  expect_s3_class(agfd[[1]], "stars")
+  expect_s3_class(agfd[[1L]], "stars")
+})
+
+test_that("read_agfd_terra returns a list of terra objects", {
+  vcr::use_cassette("_get_agfd_fixed_prices", {
+    agfd <- read_agfd_terra(fixed_prices = TRUE, yyyy = 2020:2021)
+  })
+  expect_length(agfd, 2L)
+  expect_s3_class(agfd[[1L]], "terra")
 })
