e3f961def0e2fb6779729e7298235ca70f0ae924
Adam H. Sparks <adam.sparks@curtin.edu.au>
2025-11-17 16:25:19 +0800

    edits to test files

--- tests/testthat/helper-agfd.R
+++
@@ -1,3 +0,0 @@
-locate_agfd_fixture <- function(name = "agfd_fixture_valid_nc.zip") {
-  system.file("extdata", name, package = "read.abares")
-}
---
+++ tests/testthat/helper-fixtures.R
@@ -0,0 +1,7 @@
+locate_agfd_fixture <- function(name = "agfd_fixture_valid_nc.zip") {
+  system.file("extdata", name, package = "read.abares")
+}
+
+locate_clum_fixture <- function(name = "agfd_fixture_valid_nc.zip") {
+  system.file("extdata", name, package = "read.abares")
+}
