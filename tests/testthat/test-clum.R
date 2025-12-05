2a93bc758520089d1691410d6a1329a3e42e6a43
Adam H. Sparks <adam.sparks@curtin.edu.au>
2025-12-05 15:31:51 +0800

    improved test

--- tests/testthat/test-clum.R
+++ tests/testthat/test-clum.R
@@ -2,7 +2,7 @@
 
 test_that("read_clum_stars() works properly", {
   c_stars <- read_clum_stars(zip_file, data_set = "clum_50m_2023_v2")
-  expect_equal(unname(nrow(c_stars)), 95)
+  expect_equal(unname(dim(c_stars)), c(95, 90))
   expect_named(c_stars, "clum_50m_2023_v2.tif")
 })
 
