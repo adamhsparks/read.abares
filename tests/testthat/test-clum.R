a39eed578d9e008c6e7eb2f0c3b454dc9eae75b8
Adam H. Sparks <adam.sparks@curtin.edu.au>
2025-12-08 16:30:18 +0800

    add tests for clum_terra

--- tests/testthat/test-clum.R
+++ tests/testthat/test-clum.R
@@ -13,7 +13,7 @@ create_clum_fixture <- function(zip_file) {
     files = fs::path_temp("clum_50m_2023_v2.tif")
   )
 }
-
+create_clum_fixture(zip_file)
 test_that("read_clum_stars() works properly", {
   c_stars <- read_clum_stars(zip_file, data_set = "clum_50m_2023_v2")
   expect_equal(unname(dim(c_stars)), c(95, 90))
@@ -25,4 +25,18 @@ test_that("read_clum_terra works properly", {
   c_terra <- read_clum_terra(zip_file, data_set = "clum_50m_2023_v2")
   expect_equal(dim(c_terra), c(90, 95, 1))
   expect_named(c_terra, "clum_50m_2023_v2")
+})
+
+test_that("read_clum_terra() fails with incorrect data_set", {
+  expect_error(
+    read_clum_terra(zip_file, data_set = "non_existent_data_set"),
+    "`data_set` must be one of"
+  )
+})
+
+test_that("read_clum_stars() fails with incorrect data_set", {
+  expect_error(
+    read_clum_stars(zip_file, data_set = 1L),
+    "`data_set` must be a single character string value."
+  )
 })
