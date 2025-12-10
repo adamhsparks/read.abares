7d7a5236969d6fe75d56063e7d1b701398edffd5
Adam H. Sparks <adam.sparks@curtin.edu.au>
2025-12-10 18:37:06 +0800

    more work to get nlum tests in working order

--- tests/testthat/test_nlum.R
+++ tests/testthat/test_nlum.R
@@ -1,38 +1,60 @@
 library(terra)
-nlum_file_names <- list(
-  Y202021 = "NLUM_v7_250_ALUMV8_2020_21_alb_package_20241128",
-  Y201516 = "NLUM_v7_250_ALUMV8_2015_16_alb_package_20241128",
-  Y201011 = "NLUM_v7_250_ALUMV8_2010_11_alb_package_20241128",
-  C201121 = "NLUM_v7_250_CHANGE_SIMP_2011_to_2021_alb_package_20241128",
-  T202021 = "NLUM_v7_250_INPUTS_2020_21_geo_package_20241128",
-  T201516 = "NLUM_v7_250_INPUTS_2015_16_geo_package_20241128",
-  T201011 = "NLUM_v7_250_INPUTS_2010_11_geo_package_20241128",
-  P202021 = "NLUM_v7_250_AgProbabilitySurfaces_2020_21_geo_package_20241128",
-  P201516 = "NLUM_v7_250_AgProbabilitySurfaces_2015_16_geo_package_20241128",
-  P201011 = "NLUM_v7_250_AgProbabilitySurfaces_2010_11_geo_package_20241128"
+nlum_file_names <- c(
+  "NLUM_v7_250_ALUMV8_2020_21_alb_package_20241128",
+  "NLUM_v7_250_ALUMV8_2015_16_alb_package_20241128",
+  "NLUM_v7_250_ALUMV8_2010_11_alb_package_20241128",
+  "NLUM_v7_250_CHANGE_SIMP_2011_to_2021_alb_package_20241128",
+  "NLUM_v7_250_INPUTS_2020_21_geo_package_20241128",
+  "NLUM_v7_250_INPUTS_2015_16_geo_package_20241128",
+  "NLUM_v7_250_INPUTS_2010_11_geo_package_20241128",
+  "NLUM_v7_250_AgProbabilitySurfaces_2020_21_geo_package_20241128",
+  "NLUM_v7_250_AgProbabilitySurfaces_2015_16_geo_package_20241128",
+  "NLUM_v7_250_AgProbabilitySurfaces_2010_11_geo_package_20241128"
 )
 
-zip_file <- fs::path_temp(names(nlum_file_names))
-fs::dir_create(fs::path_temp(nlum_file_names))
-nlum <- rast(system.file("ex/elev.tif", package = "terra"))
-create_nlum_fixture <- function(zip_file, nlum) {
-  names(nlum) <- zip_file
+nlum_data_sets <- c(
+  "Y202021",
+  "Y201516",
+  "Y201011",
+  "C201121",
+  "T202021",
+  "T201516",
+  "T201011",
+  "P202021",
+  "P201516",
+  "P201011"
+)
+
+zip_file <- fs::path_temp(nlum_file_names)
+fs::dir_create(zip_file)
+
+create_nlum_fixture <- function(zip_file) {
+  nlum <- rast(system.file("ex/elev.tif", package = "terra"))
+  names(nlum) <- fs::path_file(zip_file)
   writeRaster(
     nlum,
-    filename = fs::path_temp(fs::path_file(zip_file), "nlum.tif"),
+    filename = sprintf("%s/%s.tif", zip_file, fs::path_file(zip_file)),
     overwrite = TRUE
   )
   utils::zip(
     zip_file,
-    files = fs::path_temp(fs::path_file(zip_file), "nlum.tif")
+    files = fs::path(zip_file, "nlum.tif")
   )
-  fs::dir_delete(fs::path_temp("nlum"))
+  fs::dir_delete(zip_file)
 }
 
-test_that("read_clum_stars() works properly", {
-  c_stars <- read_clum_stars(x = zip_file, data_set = "nlum")
-  expect_equal(unname(dim(c_stars)), c(95, 90))
-  expect_named(c_stars, "nlum.tif")
+lapply(X = zip_file, FUN = create_nlum_fixture)
+
+nlum_file_zip <- fs::dir_ls(fs::path_temp(), glob = "*.zip")
+
+test_that("read_nlum_stars() works properly for Y201011", {
+  n_stars <- lapply(
+    X = nlum_file_zip,
+    FUN = read_nlum_stars,
+    data_set = NULL
+  )
+  expect_equal(unname(dim(n_stars)), c(95, 90))
+  expect_named(n_stars, paste0(i, ".tif"))
 })
 
 
