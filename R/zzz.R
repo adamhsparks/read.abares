7391f27a7b9fef0bd2ae75e883b7d37f16b79d21
Adam H. Sparks <adamhsparks@icloud.com>
2025-12-13 16:26:38 +0800

    add stub for testing only

--- R/zzz.R
+++ R/zzz.R
@@ -96,3 +96,10 @@
 
   invisible()
 }
+
+# This exists only for testing purposes and is not used in the package
+#' Internal retry download helper
+#' @keywords internal
+.retry_download <- function(url, dest) {
+  utils::download.file(url, dest, mode = "wb")
+}
