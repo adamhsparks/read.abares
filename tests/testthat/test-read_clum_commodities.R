e1a1f9c401c5937a2a27b24ad4539a88de428508
Adam H. Sparks <adam.sparks@curtin.edu.au>
2026-01-13 14:31:00 +0800

    skip more tests on windows due to flakey vsizip

--- tests/testthat/test-read_clum_commodities.R
+++ tests/testthat/test-read_clum_commodities.R
@@ -1,10 +1,12 @@
 test_that("read_clum_commodities returns an sf object from provided zip", {
+  skip_on_os("windows") # Skip due to intermittent vsizip issues on Windows CI
   zip_path <- test_path("testdata", "test_clum_commodities.zip")
   result <- read_clum_commodities(zip_path)
   expect_s3_class(result, "sf")
 })
 
 test_that("read_clum_commodities calls .retry_download when x is NULL", {
+  skip_on_os("windows") # Skip due to intermittent vsizip issues on Windows CI
   captured <- NULL
   zipfile <- test_path("testdata", "test_clum_commodities.zip")
 
