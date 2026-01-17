593c10a3a99de45c08a9dc77dce6323c2d62fd8e
Adam H. Sparks <adamhsparks@icloud.com>
2026-01-17 11:37:09 +0800

    skip all tests on CRAN to avoid surprises

--- tests/testthat.R
+++ tests/testthat.R
@@ -8,4 +8,11 @@
 
 library(testthat)
 library(read.abares)
-test_check("read.abares")
+
+if (Sys.getenv("NOT_CRAN") == "true") {
+  # like global skip_on_cran
+  # https://github.com/r-lib/testthat/issues/144#issuecomment-396902791
+  # according to https://github.com/hadley/testthat/issues/144
+  Sys.setenv("R_TESTS" = "")
+  test_check("read.abares")
+}
